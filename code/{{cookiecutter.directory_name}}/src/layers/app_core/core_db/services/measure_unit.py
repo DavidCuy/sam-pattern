6e49a53a49a14a84b5d7cacd479d6f61813e5216
David De Los Santos Cuy Sánchez <david.cuy.sanchez@gmail.com>
2025-01-31 23:14:00 -0600

    fix_ add isy project template config

--- code/{{cookiecutter.directory_name}}/src/layers/app_core/core_db/services/measure_unit.py
+++
@@ -1,7 +0,0 @@
-from core_db.BaseService import BaseService
-from core_db.models.measure_unit import MeasureUnit
-
-
-class MeasureUnitService(BaseService):
-    def __init__(self) -> None:
-        super().__init__(MeasureUnit)
